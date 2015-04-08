1b875818bce3abf8ee66ab3b1c60e4b9e88a41a7
Laurent Grangeau <laurent.grangeau@gmail.com>
2015-04-08 18:53:16 +0200

    Add example files

---
+++ freeze.py
@@ -0,0 +1,20 @@
+import mcpi.minecraft as minecraft
+import time
+
+mc = minecraft.Minecraft.create()
+
+while True:
+	time.sleep(0.2)
+	pos = mc.player.getPos()
+
+	x = pos.x
+	y = pos.y
+	z = pos.z
+
+	blockBelow = mc.getBlock(x, y - 1, z)
+
+	water = 9
+	ice = 79
+
+	if blockBelow == water:
+		mc.setBlock(x, y - 1, z, ice)
---
+++ teleport.py
@@ -0,0 +1,9 @@
+import mcpi.minecraft as minecraft
+
+mc = minecraft.Minecraft.create()
+
+x = 10
+y = 11
+z = 12
+
+mc.player.setPos(x, y, z)
